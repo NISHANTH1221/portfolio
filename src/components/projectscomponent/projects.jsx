e50ba396c1ef60a5eeb0c0a95176b081af3080a1
NISHANTH1221 <nb03433@gmail.com>
2024-02-07 13:35:41 +0530

    changes in images view

--- src/components/projectscomponent/projects.jsx
+++ src/components/projectscomponent/projects.jsx
@@ -4,15 +4,15 @@ import Imageslider from "./imageslider";
 function Projects(){
     const slides=[
         {
-            url:"/image-1.png",
+            url:"portfolio/image-1.png",
             title:"Discover a revolutionary platform that transforms the storage landscape in India. WarehouseHub is your go-to destination for efficient warehousing and inventory management. For customers, we offer a diverse range of warehouses categorized into Dry Freezer, and Chiller, ensuring tailored storage solutions. Partners enjoy a seamless onboarding experience, empowering them to add and manage warehouses effortlessly. Admins maintain oversight, verifying partners and facilitating collaborative inventory management. With transparent inventory tracking, users gain real-time insights and historical data for informed decision-making. WarehouseHub is not just a platform; it's a dynamic ecosystem, redefining how businesses approach storage solutions. Welcome to the future of warehousing!"
         },
         {
-            url:"/image-2.png",
+            url:"portfolio/image-2.png",
             title:"I've buit this portfolio entirely from scratch using React Js and CSS. This profile is version 1 of my portfolio and I am planning to update my website in some time. Currently working on some AI tools ideas."
         },
         {
-            url:"/image-3.jpeg",
+            url:"portfolio/image-3.jpeg",
             title:"Working on better AI projects"
         }
     ];
