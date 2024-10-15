391e45303ef8b1ab6904250e6e882169fe36a6a2
NISHANTH1221 <nb03433@gmail.com>
2024-10-16 01:23:57 +0530

    added loadng page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 const Contactme = React.lazy(()=> import("./components/contactme/contactme"));
 import {useTypewriter,cursor} from "react-simple-typewriter";
@@ -11,188 +11,208 @@ import videobg from "./assets/videobg.mp4";
 const AboutSection =  React.lazy(()=>import('./components/aboutme/aboutme'));
 const TechnicalSection = React.lazy(()=>import('./components/technicalsection/technicalsection'));
 const Projects = React.lazy(()=>import("./components/projectscomponent/projects"));
+import Loading from './components/loading/loading';
 
 
 function App() {
- const [typeEffect]=useTypewriter({
-  words:["Web Developer","Ml Enthusiast"],
-  loop:{},
-  typeSpeed:100,
-  deleteSpeed:80,
-  delaySpeed:200
- })
- const options = {
-  // Your options here, for example:
-  duration: 500,
-  smooth: true,
-};
- const isMobile = window.innerWidth <= 700;
-  return (
+
+  const [isLoading,setIsLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false)
+    },1000)
+  });
+    return (
+      <div>
+        {
+          isLoading ? <Loading /> : <MainContent />
+        }
+      </div>
+    )
+}
+
+
+const MainContent = () =>{
+  const [typeEffect]=useTypewriter({
+    words:["Web Developer","Ml Enthusiast"],
+    loop:{},
+    typeSpeed:100,
+    deleteSpeed:80,
+    delaySpeed:200
+  })
+  const options = {
+    // Your options here, for example:
+    duration: 500,
+    smooth: true,
+  };
+  const isMobile = window.innerWidth <= 700;
+  return(
     <>
-      <div className='body'>
-            <div id="home" className="intro">
-              <video src={videobg} autoPlay muted loop/>
-              <div className='overlay'></div>
-              <div className='introdetails'>
-               <p><b> I am <b>Nishanth</b></b> </p>
-               <p1>A {typeEffect}</p1>
-               <div className='introaddress'>
-                 <p><b>Delhi,India</b></p>
-               </div>
+        <div className='body'>
+              <div id="home" className="intro">
+                <video src={videobg} autoPlay muted loop/>
+                <div className='overlay'></div>
+                <div className='introdetails'>
+                <p><b> I am <b>Nishanth</b></b> </p>
+                <p1>A {typeEffect}</p1>
+                <div className='introaddress'>
+                  <p><b>Delhi,India</b></p>
+                </div>
+                </div>
               </div>
-            </div>
-          <div className='header'>
-            <div className='logo'>
-              <img src={Logo} />
-            </div>
-             {isMobile ? undefined :
-                <ul>
-                  <li>
-                    <Link
-                    to='home'
-                    smooth={true}
-                    offset={-55}
-                    duration={500}
-                    
-                    >
-                      Home
-                    </Link>
-                    </li>
-                  <li>
-                    <Link
-                    to='aboutme'
-                    smooth={true}
-                    offset={-55}
-                    duration={500}
-                    >
-                    About Me
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to='resume'
+            <div className='header'>
+              <div className='logo'>
+                <img src={Logo} />
+              </div>
+              {isMobile ? undefined :
+                  <ul>
+                    <li>
+                      <Link
+                      to='home'
+                      smooth={true}
+                      offset={-55}
+                      duration={500}
+                      
+                      >
+                        Home
+                      </Link>
+                      </li>
+                    <li>
+                      <Link
+                      to='aboutme'
                       smooth={true}
                       offset={-55}
                       duration={500}
                       >
-                       Resume
+                      About Me
                       </Link>
-                  </li>
-                  <li>
+                    </li>
+                    <li>
                       <Link
-                        to='projects'
+                        to='resume'
                         smooth={true}
                         offset={-55}
                         duration={500}
                         >
-                        Projects
-                      </Link>
+                        Resume
+                        </Link>
                     </li>
-                  <li >
-                    <a onClick={()=>animateScroll.scrollToBottom(options)}>
-                    Contact
-                    </a> 
+                    <li>
+                        <Link
+                          to='projects'
+                          smooth={true}
+                          offset={-55}
+                          duration={500}
+                          >
+                          Projects
+                        </Link>
+                      </li>
+                    <li >
+                      <a onClick={()=>animateScroll.scrollToBottom(options)}>
+                      Contact
+                      </a> 
+                    </li>
+                  </ul>
+              }
+                
+              </div>
+              <div className='socials'>
+                <ul>
+                  <li>
+                    <a href='https://www.instagram.com/nishanth.c.7/'><FaInstagram /></a>
+                  </li>
+                  <li>
+                    <a href='https://twitter.com/NishanthFolio'><FaXTwitter /></a>
+                  </li>
+                  <li>
+                    <a href='https://www.linkedin.com/in/nishanth-babu-challa/'><FaLinkedinIn /></a>
                   </li>
                 </ul>
-             }
-              
-            </div>
-            <div className='socials'>
-              <ul>
-                <li>
-                  <a href='https://www.instagram.com/nishanth.c.7/'><FaInstagram /></a>
-                </li>
-                <li>
-                  <a href='https://twitter.com/NishanthFolio'><FaXTwitter /></a>
-                </li>
-                <li>
-                  <a href='https://www.linkedin.com/in/nishanth-babu-challa/'><FaLinkedinIn /></a>
-                </li>
-              </ul>
-              <p>Follow me</p>
-            </div>
+                <p>Follow me</p>
+              </div>
 
-            <AboutSection />
-            
-            <div id="resume" className='resumesection'>
-               <div className='resumeheading'>
-                  <p>My Resume</p>
-               </div>
-               <div className='resumecontent'>
-                 <div className='workexperiencecontent'>
-                   <div className='workheading'>
-                      <p>Experience</p>
-                   </div>
-                   <div className='workyear'>
-                        <div className='circle'>1</div>
-                        <div>
-                            <p>
-                            Jun 2023- Nov 2023
-                            </p>
+              <AboutSection />
+              
+              <div id="resume" className='resumesection'>
+                <div className='resumeheading'>
+                    <p>My Resume</p>
+                </div>
+                <div className='resumecontent'>
+                  <div className='workexperiencecontent'>
+                    <div className='workheading'>
+                        <p>Experience</p>
+                    </div>
+                    <div className='workyear'>
+                          <div className='circle'>1</div>
+                          <div>
+                              <p>
+                              Jun 2023- Nov 2023
+                              </p>
+                          </div>
+                      
+                    </div>
+                    <div className='role'>
+                        <div className='rolename'>
+                          <p>Web Developer Intern</p>
                         </div>
-                     
-                   </div>
-                   <div className='role'>
-                      <div className='rolename'>
-                        <p>Web Developer Intern</p>
-                      </div>
-                      <div className='companyname'>
-                         Algomatix Technologies Pvt Ltd
-                      </div>
-                      <div className='rolework'>
-                        <p1>I've created a website which will be used by the company to rent out warehouse's space under their command</p1>
-                      </div>
-                   </div>
-                 </div>
-                 <div className='educationcontent'>
-                     <div className='educationheading'>
-                        <p>Education</p>
-                     </div>
-                     <div className='workyear'>
-                        <div className='circle'>1</div>
-                        <div>
-                            <p>
-                            2021-2025
-                            </p>
+                        <div className='companyname'>
+                          Algomatix Technologies Pvt Ltd
                         </div>
-                     
-                   </div>
-                   <div className='role'>
-                      <div className='rolename'>
-                        <p>Electronics and Communication Engineering</p>
-                      </div>
-                      <div className='companyname'>
-                         National Institute of Technology, Delhi
-                      </div>
-                      <div className='rolework'>
-                        <p1>CGPA : 8.08</p1>
-                      </div>
-                   </div>
-                   <div className='workyear'>
-                        <div className='circle'>2</div>
-                        <div>
-                            <p>
-                            2023-2025
-                            </p>
+                        <div className='rolework'>
+                          <p1>I've created a website which will be used by the company to rent out warehouse's space under their command</p1>
                         </div>
-                     
-                   </div>
-                   <div className='role'>
-                      <div className='rolename'>
-                        <p>AI and ML (Minor Degree)</p>
+                    </div>
+                  </div>
+                  <div className='educationcontent'>
+                      <div className='educationheading'>
+                          <p>Education</p>
                       </div>
-                      <div className='companyname'>
-                         <p>National Institute of Technology,Delhi</p>
-                      </div>
-                   </div>
-                 </div>
-               </div>
-            </div>
-            <TechnicalSection />
-            <Projects id="projects"/>
-            <Contactme id="contact"/>
-      </div>
-    </>
+                      <div className='workyear'>
+                          <div className='circle'>1</div>
+                          <div>
+                              <p>
+                              2021-2025
+                              </p>
+                          </div>
+                      
+                    </div>
+                    <div className='role'>
+                        <div className='rolename'>
+                          <p>Electronics and Communication Engineering</p>
+                        </div>
+                        <div className='companyname'>
+                          National Institute of Technology, Delhi
+                        </div>
+                        <div className='rolework'>
+                          <p1>CGPA : 8.08</p1>
+                        </div>
+                    </div>
+                    <div className='workyear'>
+                          <div className='circle'>2</div>
+                          <div>
+                              <p>
+                              2023-2025
+                              </p>
+                          </div>
+                      
+                    </div>
+                    <div className='role'>
+                        <div className='rolename'>
+                          <p>AI and ML (Minor Degree)</p>
+                        </div>
+                        <div className='companyname'>
+                          <p>National Institute of Technology,Delhi</p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <TechnicalSection />
+              <Projects id="projects"/>
+              <Contactme id="contact"/>
+        </div>
+      </>
   )
 }
 
---
+++ src/components/loading/loading.jsx
@@ -0,0 +1,27 @@
+import "./loading.css"
+const Loading = () => {
+    return (
+        <div className="loading">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="100" height="100">
+                    <circle fill="white" stroke="#FF156D" stroke-width="15" r="15" cx="35" cy="100">
+                        <animate attributeName="cx" calcMode="spline" dur="2" values="35;165;165;35;35" keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1" repeatCount="indefinite" begin="0"></animate>
+                    </circle>
+                    <circle fill="white" stroke="#FF156D" stroke-width="15" opacity=".8" r="15" cx="35" cy="100">
+                        <animate attributeName="cx" calcMode="spline" dur="2" values="35;165;165;35;35" keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1" repeatCount="indefinite" begin="0.05"></animate>
+                    </circle>
+                    <circle fill="white" stroke="#FF156D" stroke-width="15" opacity=".6" r="15" cx="35" cy="100">
+                        <animate attributeName="cx" calcMode="spline" dur="2" values="35;165;165;35;35" keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1" repeatCount="indefinite" begin=".1"></animate>
+                    </circle>
+                    <circle fill="white" stroke="#FF156D" stroke-width="15" opacity=".4" r="15" cx="35" cy="100">
+                        <animate attributeName="cx" calcMode="spline" dur="2" values="35;165;165;35;35" keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1" repeatCount="indefinite" begin=".15"></animate>
+                    </circle>
+                    <circle fill="white" stroke="#FF156D" stroke-width="15" opacity=".2" r="15" cx="35" cy="100">
+                        <animate attributeName="cx" calcMode="spline" dur="2" values="35;165;165;35;35" keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1" repeatCount="indefinite" begin=".2"></animate>
+                    </circle>
+                </svg>
+           <div>Loading Best Experience ...</div>
+        </div>
+    );
+}
+ 
+export default Loading;
